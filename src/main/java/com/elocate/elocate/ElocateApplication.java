20a785c0b34563c1c77d5ceb056dd64dcf829498
sumalesh-darpg <sumalesh.sumalesh@accenture.com>
2025-11-25 23:06:51 +0530

    Initial commit

---
+++ src/main/java/com/elocate/elocate/ElocateApplication.java
@@ -0,0 +1,13 @@
+package com.elocate.elocate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElocateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElocateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elocate/elocate/ElocateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elocate.elocate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElocateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
