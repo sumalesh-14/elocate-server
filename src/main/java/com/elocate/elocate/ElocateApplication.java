ebf5509c1ef19ff05e77f901c1c13b161235cd93
sumalesh-darpg <sumalesh.sumalesh@accenture.com>
2026-03-07 12:01:41 +0530

    fix

--- src/main/java/com/elocate/elocate/ElocateApplication.java
+++ src/main/java/com/elocate/elocate/ElocateApplication.java
@@ -2,7 +2,9 @@ package com.elocate.elocate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +12,11 @@ public class ElocateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElocateApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
